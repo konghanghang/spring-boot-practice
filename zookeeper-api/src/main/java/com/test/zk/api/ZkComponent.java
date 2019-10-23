193c7d856db6cc6d598819ca81937fb28ca84eb7
konghanghang <yslao@outlook.com>
2019-10-23 15:33:09 +0800

    zookeeper的api完善

--- zookeeper-api/src/main/java/com/test/zk/api/ZkComponent.java
+++ zookeeper-api/src/main/java/com/test/zk/api/ZkComponent.java
@@ -1,6 +1,8 @@
 package com.test.zk.api;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.hibernate.validator.constraints.EAN;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * api 测试
@@ -34,7 +38,7 @@ public class ZkComponent {
             zooKeeper=new ZooKeeper(servers, 5000, event -> {
                 /** 判断是否和服务器之间取得了连接*/
                 if(event.getState()== Watcher.Event.KeeperState.SyncConnected){
-                    logger.info("已经触发了{}事件！", event.getType());
+                    logger.info("已经触发了{}事件！", event);
                 }
             });
         } catch (IOException e) {
@@ -43,15 +47,88 @@ public class ZkComponent {
         logger.info("zk init end...");
     }
 
-    public void createNode(){
-        String path = "/api_test";
-        String nodePath = null;
+    /**
+     * 创建路径和节点
+     * @param path      全路径
+     * @param value     节点值
+     * @param pathMode  创建路径模式
+     * @param nodeMode  创建节点模式
+     * @return
+     */
+    public boolean createPath(String path, String value, CreateMode pathMode, CreateMode nodeMode){
+        String[] pathArray = path.split("/");
+        String tempPath = "/";
+        for (int i = 1; i < pathArray.length; i++) {
+            if (i == 1){
+                tempPath = tempPath + pathArray[i];
+            } else {
+                tempPath = tempPath + "/" + pathArray[i];
+            }
+            try {
+                if (zooKeeper.exists(tempPath, false) == null){
+                    String nodePath = null;
+                    if (i == pathArray.length - 1){
+                        nodePath = zooKeeper.create(tempPath, value.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, nodeMode);
+                    } else {
+                        nodePath = zooKeeper.create(tempPath, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, pathMode);
+                    }
+                    logger.info("create path:{} success", nodePath);
+                }
+            } catch (KeeperException | InterruptedException e) {
+                logger.error("create path:{} error, msg:{}", tempPath, e.getMessage());
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 获得子节点
+     * @param path
+     * @return
+     */
+    public List<String> getChildren(String path){
+        List<String> list = new ArrayList<>();
         try {
-            nodePath = zooKeeper.create(path, "123".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            list.addAll(zooKeeper.getChildren(path, false));
         } catch (KeeperException | InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println(nodePath);
+        return list;
+    }
+
+    /**
+     * 获取节点数据
+     * @param path
+     * @return
+     */
+    public String getData(String path){
+        try {
+            return new String(zooKeeper.getData(path, false, null));
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 设置值
+     * @param path
+     * @param data
+     * @return
+     */
+    public boolean setData(String path, String data) {
+        try {
+            /**
+             * version -1不检测dataVersion, 如果要检测，改值传递查询出来的dataVersion既可
+             */
+            Stat stat = zooKeeper.setData(path, data.getBytes(), -1);
+        } catch (KeeperException | InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
 }
--- zookeeper-api/src/test/java/com/test/zk/api/ApiTest.java
+++ zookeeper-api/src/test/java/com/test/zk/api/ApiTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,8 +19,20 @@ public class ApiTest {
     private ZkComponent zkComponent;
 
     @Test
-    public void createNode(){
-        zkComponent.createNode();
+    public void createPath(){
+        zkComponent.createPath("/my/zk/test/node", "node1", CreateMode.PERSISTENT, CreateMode.PERSISTENT_SEQUENTIAL);
+    }
+
+    @Test
+    public void getChildren(){
+        String path = "/my/zk/test";
+        List<String> children = zkComponent.getChildren(path);
+        children.stream().forEach(child -> System.out.println(zkComponent.getData(path + "/" + child)));
+    }
+
+    @Test
+    public void setData() {
+        boolean oneTwoThree = zkComponent.setData("/my/zk/test/node0000000001", "oneTwoThree");
     }
 
 }
