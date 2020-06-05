a26e7702bfc6db36b5d0de13d46e0d49ba9adc70
konghanghang <yslao@outlook.com>
2020-06-05 18:43:26 +0800

    spark连接hadoop hdfs

--- spark/src/main/java/com/test/App.java
+++ spark/src/main/java/com/test/App.java
@@ -3,15 +3,19 @@ package com.test;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
 
 public class App {
 
     public static void main(String[] args) {
         SparkConf conf = new SparkConf()
-                .setAppName("my-test");
+                .setAppName("my-test").setMaster("local[*]");
 
         JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> lines = sc.textFile("hdfs://hadoop01:9000/a.txt");
+        JavaRDD<String> lines = sc.textFile("hdfs://DESKTOP-67PPMIS.localdomain:9000/hello/word/README.txt");
+        System.out.println("---------------------");
+        lines.foreach((VoidFunction<String>) s -> System.out.println(s));
+        System.out.println("---------------------");
     }
 
 }
