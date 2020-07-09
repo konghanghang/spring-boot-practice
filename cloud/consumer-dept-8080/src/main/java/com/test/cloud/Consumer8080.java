4514bf4a37bd38078e54fa9b1a93d315da97b37b
konghanghang <yslao@outlook.com>
2020-07-09 13:38:51 +0800

    改变ribbon访问规则为随机访问

--- cloud/consumer-dept-8080/src/main/java/com/test/cloud/Consumer8080.java
+++ cloud/consumer-dept-8080/src/main/java/com/test/cloud/Consumer8080.java
@@ -1,11 +1,14 @@
 package com.test.cloud;
 
+import com.test.cloud.config.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "provider-dept", configuration = MySelfRule.class)
 public class Consumer8080 {
 
     public static void main(String[] args) {
