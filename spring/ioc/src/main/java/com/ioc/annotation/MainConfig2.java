8d93831f22189990480e815e8e3a26cf44baf091
konghanghang <yslao@outlook.com>
2020-12-20 10:10:10 +0800

    bean懒加载

--- spring/ioc/src/main/java/com/ioc/annotation/MainConfig2.java
+++ spring/ioc/src/main/java/com/ioc/annotation/MainConfig2.java
@@ -3,6 +3,7 @@ package com.ioc.annotation;
 import com.ioc.Person;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
@@ -11,11 +12,14 @@ public class MainConfig2 {
     /**
      * singleton 单实例（默认值） ioc容器启动会去调用方法创建对象放到ioc容器中，以后每次使用都去ioc容器中获取
      * prototype 多实例         ioc容器启动不会去调用方法创建对象，每次使用时才会调用方法获取对象，每次都是一个新对象
+     * @Lazy 容器创建的时候不创建对象，在第一次使用时创建对象
      * @return
      */
-    @Scope
+    @Lazy
+    @Scope("singleton")
     @Bean
     public Person person(){
+        System.out.println("向ioc中添加bean。。。。");
         return new Person("haha", 12);
     }
 
