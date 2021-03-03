be65d60dfa87507adaad15d8763755e9b5b57ee3
konghanghang <yslao@outlook.com>
2021-03-03 18:34:10 +0800

    @FeignClient中的@RequestMapping也被SpringMVC加载的问题解决

---
+++ cloud/consumer-dept-feign-8080/src/main/java/com/test/cloud/config/FeignConfiguration.java
@@ -0,0 +1,38 @@
+package com.test.cloud.config;
+
+import feign.Feign;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+/**
+ * FeignClient中的@RequestMapping也被SpringMVC加载的问题解决
+ * @author yslao@outlook.com
+ * @since 2021/3/3
+ */
+@Configuration
+@ConditionalOnClass({Feign.class})
+public class FeignConfiguration {
+
+    @Bean
+    public WebMvcRegistrations feignWebRegistrations() {
+        return new WebMvcRegistrations() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new FeignRequestMappingHandlerMapping();
+            }
+        };
+    }
+
+    private static class FeignRequestMappingHandlerMapping extends RequestMappingHandlerMapping {
+        @Override
+        protected boolean isHandler(Class<?> beanType) {
+            return super.isHandler(beanType) &&
+                    !AnnotatedElementUtils.hasAnnotation(beanType, FeignClient.class);
+        }
+    }
+}
