0e72a8f970692fba2d1affa0bd7faaa20fe34b56
konghanghang <yslao@outlook.com>
2021-03-26 18:54:29 +0800

    自定义feign调用-未完成

---
+++ cloud/nacos-consumer-8080/src/main/java/com/test/feign/InvokeContent.java
@@ -0,0 +1,22 @@
+package com.test.feign;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 保存需要使用的数据
+ * @author yslao@outlook.com
+ * @since 2021/3/26
+ */
+public class InvokeContent {
+
+    private Map<String, String[]> parameterNamesMap = new ConcurrentHashMap<>();
+
+    public String[] getParameterNames(String key) {
+        return this.parameterNamesMap.get(key);
+    }
+
+    public void setParametersName(String key, String[] parameterNames) {
+        this.parameterNamesMap.put(key, parameterNames);
+    }
+}
