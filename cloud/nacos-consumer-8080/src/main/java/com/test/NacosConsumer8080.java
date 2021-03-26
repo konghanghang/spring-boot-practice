fc5d7e956101ba53d15b5eb5f60c8d31768a3920
konghanghang <yslao@outlook.com>
2021-03-26 16:22:26 +0800

    目录重构,基础依赖引用

--- cloud/nacos-consumer-8080/src/main/java/com/test/NacosConsumer8080.java
+++ cloud/nacos-consumer-8080/src/main/java/com/test/NacosConsumer8080.java
@@ -2,9 +2,10 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class NacosConsumer8080 {
 
