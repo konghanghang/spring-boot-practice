0afbf48b76301a3e321f958b22b2996000037d19
konghanghang <yslao@outlook.com>
2021-03-31 08:38:01 +0800

    二叉排序树删除节点

--- data/structure/src/main/java/com/test/tree/sort/BinarySortTree.java
+++ data/structure/src/main/java/com/test/tree/sort/BinarySortTree.java
@@ -2,6 +2,7 @@ package com.test.tree.sort;
 
 import com.test.tree.BinaryTreeNode;
 
+import java.util.Objects;
 import java.util.Stack;
 
 /**
@@ -13,6 +14,10 @@ public class BinarySortTree {
 
     private BinaryTreeNode root;
 
+    /**
+     * 添加节点
+     * @param node
+     */
     public void add(BinaryTreeNode node) {
         if (root == null) {
             root = node;
@@ -40,8 +45,161 @@ public class BinarySortTree {
         }
     }
 
+    /**
+     * 查看待删除的节点
+     * @param value
+     * @return
+     */
+    public BinaryTreeNode search(int value) {
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        stack.push(root);
+        BinaryTreeNode result = null;
+        while (!stack.isEmpty()) {
+            BinaryTreeNode pop = stack.pop();
+            if (pop != null) {
+                if (pop.getData() == value) {
+                    result = pop;
+                } else if (pop.getData() > value) {
+                    stack.push(pop.getLeft());
+                } else if (pop.getData() < value) {
+                    stack.push(pop.getRight());
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 查看待删除的节点的父节点
+     * @param value
+     * @return
+     */
+    public BinaryTreeNode searchParent(int value) {
+        // 先判断是不是根节点
+        if (root == null) {
+            return null;
+        }
+        if (root.getData() == value) {
+            return null;
+        }
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        stack.push(root);
+        BinaryTreeNode result = null;
+        while (!stack.isEmpty()) {
+            BinaryTreeNode pop = stack.pop();
+            if (pop != null) {
+                if ((pop.getLeft() != null && pop.getLeft().getData() == value)
+                        || (pop.getRight() != null && pop.getRight().getData() == value)) {
+                    result = pop;
+                } else if (pop.getData() > value) {
+                    stack.push(pop.getLeft());
+                } else if (pop.getData() < value) {
+                    stack.push(pop.getRight());
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 查看待删除的节点的父节点
+     * @param value
+     * @return
+     */
+    public void deleteNode(int value) {
+        BinaryTreeNode search = search(value);
+        // search为null，表明没有查找到要删除的节点
+        if (Objects.isNull(search)) {
+            System.out.println("未找到要删除的节点：" + value);
+            return;
+        }
+        // 找到了要删除的节点后，查找要删除的节点的父节点
+        BinaryTreeNode parent = searchParent(value);
+        // 如果父节点是空，则表明要删除的节点是根节点
+        if (Objects.isNull(parent)) {
+            // 判断根节点的子树情况,这里只考虑没有左子树和右子树的情况
+            // 其他只有一个子树或有两棵子树交给下边的统一进行删除
+            // 如果根节点没有左子树和右子树
+            if (search.getLeft() == null && search.getRight() == null) {
+                root = null;
+                return;
+            } else if (search.getLeft() != null && search.getRight() == null) {
+                root = search.getLeft();
+                return;
+            }
+            else if (search.getLeft() == null && search.getRight() != null) {
+                root = search.getRight();
+                return;
+            }
+        }
+        // 判断要删除的节点是否是叶子节点
+        if (search.getLeft() == null && search.getRight() == null) {
+            if (parent.getLeft() != null && parent.getLeft().getData() == value) {
+                parent.setLeft(null);
+            } else if (parent.getRight() != null && parent.getRight().getData() == value) {
+                parent.setRight(null);
+            }
+        } else if (search.getLeft() != null && search.getRight() != null) {
+            // 删除的节点有左右两棵子树
+            // 查找待删除节点的右子树的最小值
+            // int searchValue = deleteTreeMinOrMax(search.getRight(), 1);
+            // 查找待删除节点的左子树的最大值
+             int searchValue = deleteTreeMinOrMax(search.getLeft(), 2);
+            search.setData(searchValue);
+        } else {
+            // 删除的节点有一个子树
+            // 一个子树是左子节点
+            if (search.getLeft() != null) {
+                if (parent.getLeft().getData() == value) {
+                    parent.setLeft(search.getLeft());
+                } else if (parent.getRight().getData() == value) {
+                    parent.setRight(search.getLeft());
+                }
+            } else {
+                //一个子树是右子节点
+                if (parent.getLeft().getData() == value) {
+                    parent.setLeft(search.getRight());
+                } else if (parent.getRight().getData() == value) {
+                    parent.setRight(search.getRight());
+                }
+            }
+        }
+    }
+
+    /**
+     * 删除右子树中最小的节点
+     * 在进行删除两个子树的节点时，我们有两种操作方法
+     * 1. 查找右子树的最小值，并在树中删除它，然后把要删除的节点的值赋值成已删除的节点的值
+     * 2. 查找左子树的最大值，并在树中删除它，然后把要删除的节点的值赋值成已删除的节点的值
+     * @param node
+     * @param type 1:第一种情况 2:第二种情况
+     * @return 返回已删除的最小节点值
+     */
+    public int deleteTreeMinOrMax(BinaryTreeNode node, int type) {
+        BinaryTreeNode target = node;
+        if (type == 1) {
+            // 向右找，找最小
+            while (target.getLeft() != null) {
+                target = target.getLeft();
+            }
+        } else if (type == 2) {
+            // 向左找，找最大
+            while (target.getRight() != null) {
+                target = target.getRight();
+            }
+        }
+        // 走出while的时候就已经找到了
+        // 删除最小的节点
+        deleteNode(target.getData());
+        return target.getData();
+    }
+
+    /**
+     * 中序遍历
+     */
     public void midOrder() {
         if (root == null) {
+            System.out.println("树为空，无法遍历");
             return;
         }
         BinaryTreeNode temp = root;
@@ -56,6 +214,7 @@ public class BinarySortTree {
                 temp = pop.getRight();
             }
         }
+        System.out.println();
     }
 
 }
--- data/structure/src/test/java/com/test/tree/sort/BinarySortTreeTest.java
+++ data/structure/src/test/java/com/test/tree/sort/BinarySortTreeTest.java
@@ -4,8 +4,6 @@ import com.test.tree.BinaryTreeNode;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * @author yslao@outlook.com
  * @since 2021/3/30
@@ -23,6 +21,78 @@ class BinarySortTreeTest {
             binarySortTree.add(new BinaryTreeNode(i));
         }
 
+        binarySortTree.midOrder();
+    }
+
+    /**
+     * 下边几个方法的树形状如下：
+     *              7
+     *            |    \
+     *          3       10
+     *        |  \     |  \
+     *       1    5   9    12
+     *        \
+     *         2
+     */
+    @DisplayName("查找排序二叉树节点")
+    @Test
+    void searchSortTree() {
+        BinarySortTree binarySortTree = new BinarySortTree();
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
+
+        for (int i : arr) {
+            binarySortTree.add(new BinaryTreeNode(i));
+        }
+
+        binarySortTree.midOrder();
+
+        int value = 1;
+        BinaryTreeNode search = binarySortTree.search(value);
+        System.out.println();
+        System.out.println("查找到待删除节点：");
+        System.out.println(search);
+        BinaryTreeNode parent = binarySortTree.searchParent(value);
+        System.out.println("查找到父节点：");
+        System.out.println(parent);
+    }
+
+    @DisplayName("删除排序二叉树节点")
+    @Test
+    void deleteSortTreeLeafNode() {
+        BinarySortTree binarySortTree = new BinarySortTree();
+        int[] arr = {7, 3, 10, 12, 5, 1, 9, 2};
+
+        for (int i : arr) {
+            binarySortTree.add(new BinaryTreeNode(i));
+        }
+
+        binarySortTree.midOrder();
+        System.out.println();
+        // 删除叶子节点
+        /*int value = 12;
+        binarySortTree.deleteNode(value);*/
+        // 删除只有一个子树的节点
+        /*int value = 1;
+        binarySortTree.deleteNode(value);*/
+        // 删除有两个子树的节点
+        /*int value = 3;
+        binarySortTree.deleteNode(value);*/
+        // 乱序删除
+        binarySortTree.deleteNode(2);
+        binarySortTree.midOrder();
+        binarySortTree.deleteNode(5);
+        binarySortTree.midOrder();
+        binarySortTree.deleteNode(7);
+        binarySortTree.midOrder();
+        binarySortTree.deleteNode(12);
+        binarySortTree.midOrder();
+        binarySortTree.deleteNode(10);
+        binarySortTree.midOrder();
+        binarySortTree.deleteNode(3);
+        binarySortTree.midOrder();
+        binarySortTree.deleteNode(9);
+        binarySortTree.midOrder();
+        binarySortTree.deleteNode(1);
         binarySortTree.midOrder();
     }
 }
