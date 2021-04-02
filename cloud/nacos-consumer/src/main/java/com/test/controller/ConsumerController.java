3c1466fddc59929edafea2721bce83901afab02e
konghanghang <yslao@outlook.com>
2021-04-02 16:29:23 +0800

    模块优化

--- cloud/nacos-consumer/src/main/java/com/test/controller/ConsumerController.java
+++ cloud/nacos-consumer/src/main/java/com/test/controller/ConsumerController.java
@@ -1,13 +1,19 @@
 package com.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RefreshScope
 public class ConsumerController {
+
+    @Value("${config.info:}")
+    private String info;
 
     @Autowired
     private RestTemplate restTemplate;
