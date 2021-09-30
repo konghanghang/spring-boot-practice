61e09a602d30a6efa64ff9f299ec9ffad8ee9ba3
konghanghang <yslao@outlook.com>
2021-09-30 09:41:14 +0800

    引用基础jar，删除无用类，验证knife4j

--- web-mvc/src/main/java/com/test/controller/IndexController.java
+++ web-mvc/src/main/java/com/test/controller/IndexController.java
@@ -1,38 +1,40 @@
 package com.test.controller;
 
-import com.test.annotation.Login;
 import com.test.vo.AccountModel;
 import com.test.vo.TestVo;
 import com.test.vo.UserVo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "首页模块")
 @RestController
 @RequestMapping
 public class IndexController {
 
     @RequestMapping("/index")
+    @ApiOperation(value = "首页", httpMethod = "GET")
     public String index(UserVo name) {
         System.out.println("index controller");
         return "index";
     }
 
     @PostMapping("/paramsMapTest")
+    @ApiOperation(value = "参数测试")
     public Object paramsMapTest(@RequestParam("myName") String myName,
                                 @RequestParam("myAge") int myAge){
         return String.format("name = %s,age = %d", myName, myAge);
     }
 
     @PostMapping("/modelAttributeTest")
+    @ApiOperation(value = "model参数测试")
     public Object modelAttributeTest(AccountModel accountModel){
         return accountModel.toString();
     }
 
-    @RequestMapping("/customResovlerTest")
-    @Login
-    public Object customResovlerTest(AccountModel accountModel, TestVo testVo){
+    @RequestMapping(value = "/customResovlerTest")
+    @ApiOperation(value = "自定义参数测试", httpMethod = "POST")
+    public Object customResovlerTest(@RequestBody AccountModel accountModel, TestVo testVo){
         return accountModel.toString() + "---" + testVo.toString();
     }
 
